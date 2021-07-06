0efd04aa09b8f14525beaf40014e9a65eb89e837
TomatoT1 <86846293+TomatoT1@users.noreply.github.com>
2021-07-06 19:48:28 +0000

    I added the main part of the game

--- main.py
+++ main.py
@@ -1,10 +1,11 @@
 import random
 secret = random.randint(1,50)
-guess = 0
 win = False
 loss = False
 play = True
 Tries = 5
 while play == True:
   while Tries > 0:
-    
+    print()
+    guess = int(input("what's ur number guess?(0-50): "))
+    Tries -= 1

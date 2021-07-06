da6b55dd7a05ce7d1a89ab6645087679da8f50b1
TomatoT1 <86846293+TomatoT1@users.noreply.github.com>
2021-07-06 20:09:34 +0000

    Further improvments, guesses system cut for ease for now

--- main.py
+++ main.py
@@ -5,16 +5,16 @@ loss = False
 play = True
 Tries = 5
 while play == True:
-  while Tries > 0:
-    print()
-    guess = int(input("what's ur number guess?(0-50): "))
+  print()
+  guess = int(input("what's ur number guess?(0-50): "))
+  if guess < secret:
+    print("too smol")
     Tries -= 1
-    if guess < secret:
-      print("too smol")
-    if guess > secret:
-      print("too big")
-    if guess == secret:
-      print("GG")
-      play = False
+  if guess > secret:
+    print("too big")
+    Tries -= 1
+  if guess == secret:
+    print("GG")
+    play = False
 if Tries == 0:
   print("you lost, too many tries use lol...")

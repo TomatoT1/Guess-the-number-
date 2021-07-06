208537693309d96dffe06fb88a4e776eb63c85d9
TomatoT1 <86846293+TomatoT1@users.noreply.github.com>
2021-07-06 20:01:14 +0000

    Will tell you where your number is in relation to the real number

--- main.py
+++ main.py
@@ -9,3 +9,12 @@ while play == True:
     print()
     guess = int(input("what's ur number guess?(0-50): "))
     Tries -= 1
+    if guess < secret:
+      print("too smol")
+    if guess > secret:
+      print("too big")
+    if guess == secret:
+      print("GG")
+      play = False
+if Tries == 0:
+  print("you lost, too many tries use lol...")
